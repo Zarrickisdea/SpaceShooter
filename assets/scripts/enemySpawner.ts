65aec99b26084ea3a5c51ed41dd418b9ef508e7e
Zarrickisdea <mohammedraz45@yahoo.com>
2024-01-11 23:44:26 +0530

    Procedure start

--- assets/scripts/enemySpawner.ts
+++ assets/scripts/enemySpawner.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, instantiate, Prefab, random } from 'cc';
+import { _decorator, Component, Node, instantiate, Prefab, random, Layout } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('enemySpawner')
@@ -10,20 +10,37 @@ export class enemySpawner extends Component {
         @property({type: Prefab})
         private enemyPrefabs: Prefab[] = [];
 
-        private skipRow: boolean = false;
+        @property({range: [0, 100]})
+        private skipRowChance: number = 0;
 
-        private skipRowChance: number = 0.9;
+        @property({range: [0, 100]})
+        private skipPatternChance: number = 0;
+
+        private skipRow: boolean = false;
+        private skipPattern: boolean = false;
+
+        @property({type: Layout})
+        private rowLayout: Layout = null;
     
         private spawnEnemies() {
-        this.skipRow = random() < this.skipRowChance;
-            if (!this.skipRow) {
-                console.log("Spawn Enemies");
-            for (let i = 0; i < this.enemyPrefabs.length; ++i) {
-                this.scheduleOnce(() => {
-                    this.spawnEnemy(i);
-                }, this.spawnInterval * i);
+            let shipNumber = this.enemyPrefabs.length;
+            this.skipRow = random() < (this.skipRowChance / 100);
+            this.skipPattern = random() < (this.skipPatternChance / 100);
+
+            console.log("skipPattern: " + this.skipPattern);
+
+            if (this.skipPattern) {
+                shipNumber = shipNumber / 2;
+                this.rowLayout.spacingX = this.rowLayout.spacingX * 6;
             }
-        }
+
+            if (!this.skipRow) {
+                for (let i = 0; i < shipNumber; ++i) {
+                    this.scheduleOnce(() => {
+                        this.spawnEnemy(i);
+                    }, this.spawnInterval * i);
+                }
+            }
         }
 
         private spawnEnemy(index: number) {
@@ -32,11 +49,15 @@ export class enemySpawner extends Component {
         }
     
         protected onLoad() {
-            this.spawnEnemies();
+            if (!this.rowLayout) {
+                console.warn("enemySpawner should be attached to a Layout node");
+                this.enabled = false;
+                return;
+            }
         }
     
         protected start() {
-    
+            this.spawnEnemies();
         }
     
         protected update(dt) {
