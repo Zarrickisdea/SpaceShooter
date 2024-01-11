65e2183634b104327fd73332d402fd8f53c7f819
Zarrickisdea <mohammedraz45@yahoo.com>
2024-01-11 20:21:34 +0530

    Added rows for testing

--- assets/scripts/enemySpawner.ts
+++ assets/scripts/enemySpawner.ts
@@ -1,14 +1,47 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, instantiate, Prefab, random } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('enemySpawner')
 export class enemySpawner extends Component {
-    start() {
+    
+        @property
+        private spawnInterval: number = 0;
+    
+        @property({type: Prefab})
+        private enemyPrefabs: Prefab[] = [];
 
-    }
+        private skipRow: boolean = false;
 
-    update(deltaTime: number) {
-        
-    }
+        private skipRowChance: number = 0.9;
+    
+        private spawnEnemies() {
+        this.skipRow = random() < this.skipRowChance;
+            if (!this.skipRow) {
+                console.log("Spawn Enemies");
+            for (let i = 0; i < this.enemyPrefabs.length; ++i) {
+                this.scheduleOnce(() => {
+                    this.spawnEnemy(i);
+                }, this.spawnInterval * i);
+            }
+        }
+        }
+
+        private spawnEnemy(index: number) {
+            let enemy = instantiate(this.enemyPrefabs[index]);
+            enemy.setParent(this.node);
+        }
+    
+        protected onLoad() {
+            this.spawnEnemies();
+        }
+    
+        protected start() {
+    
+        }
+    
+        protected update(dt) {
+            
+        }
 }
-
+
+
