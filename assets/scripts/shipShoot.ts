da69ad87bfd633246aaaee3a4214e4729405e960
Zarrickisdea <mohammedraz45@yahoo.com>
2024-01-13 18:44:58 +0530

    Enemy ship shooting

--- assets/scripts/shipShoot.ts
+++ assets/scripts/shipShoot.ts
@@ -1,14 +1,30 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, instantiate } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('shipShoot')
 export class shipShoot extends Component {
-    start() {
+    @property
+    private shootInterval: number = 0;
+
+    private shootBullet() {
+        let bullet = this.node.children[0];
+        bullet.active = true;
+    }
+
+    protected onLoad() {
 
     }
 
-    update(deltaTime: number) {
-        
+    protected start() {
+
+    }
+
+    protected update(deltaTime: number) {
+        this.scheduleOnce(() => {
+            this.shootBullet();
+        }, this.shootInterval);
+
     }
 }
-
+
+
